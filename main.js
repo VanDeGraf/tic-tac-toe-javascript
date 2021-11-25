2e1c3617f4bd0ea1185df14916d6d8f4606c346e
VanDeGraf <vandegraf@bk.ru>
2021-11-25 16:26:00 +0400

    Add general app structure

---
+++ main.js
@@ -0,0 +1,13 @@
+const Player = (name, mark) => {
+    const getName = () => name
+    const getMark = () => mark
+    return {getName, getMark}
+}
+
+const GameBoard = (() => {
+
+})()
+
+const DisplayController = (() => {
+
+})()
